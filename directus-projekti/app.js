89590c062b679e0a67d10c335b3879938be8ece6
katalav <194138161+katalav@users.noreply.github.com>
2025-10-31 23:28:36 +0200

    sain tehtävän näkyyn directusta..

--- directus-projekti/app.js
+++ directus-projekti/app.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 
-const authRoutes = require("./routes/auth");
-const taskRoutes = require('./routes/tasks');
 
 const app = express();
 const PORT = 3000;
  
 app.use(cors());
 app.use(express.json());
-app.use("/auth", authRoutes);
-app.use('/tasks', taskRoutes);
  
 
 app.get('/', (req, res) => {
